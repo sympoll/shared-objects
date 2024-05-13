40ba7f1f947d50077d8752f65ce6b460cac6c0c5
Ronenii <ronen.glm@gmail.com>
2024-05-13 15:22:59 +0300

    Basic implementation of poll

--- src/main/java/com/MTAPizza/Sympoll/poll/Poll.java
+++ src/main/java/com/MTAPizza/Sympoll/poll/Poll.java
@@ -1,88 +1,26 @@
 package com.MTAPizza.Sympoll.poll;
 
 import com.MTAPizza.Sympoll.answer.Answer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 
 import java.util.List;
 
 public class Poll {
-    private PostgresPoll postgresPoll;
+    private int pollID;
+    private String title;
+    private String description;
+    private int creatorID;
+    private int groupID;
+    private int votingID;
+    private int numAnswersAllowed;
+    private long timeCreated;
+    private long timeUpdated;
+    private long timeEnds;
     private List<Answer> answersList;
 
-    public Poll(PostgresPoll postgresPoll, List<Answer> answersList) {
-        this.postgresPoll = postgresPoll;
+    public Poll(List<Answer> answersList) {
         this.answersList = answersList;
-    }
-
-    public int getPollID() {
-        return postgresPoll.getPollID();
-    }
-
-    public void setPollID(int pollID) {
-        this.postgresPoll.setPollID(pollID);
-    }
-
-    public String getTitle() {
-        return postgresPoll.getTitle();
-    }
-
-    public void setTitle(String title) {
-        this.postgresPoll.setTitle(title);
-    }
-
-    public String getDescription() {
-        return postgresPoll.getDescription();
-    }
-
-    public void setDescription(String description) {
-        this.postgresPoll.setDescription(description);
-    }
-
-    public String getCreator() {
-        return postgresPoll.getCreator();
-    }
-
-    public void setCreator(String creator) {
-        this.postgresPoll.setCreator(creator);
-    }
-
-    public int getGroupID() {
-        return postgresPoll.getGroupID();
-    }
-
-    public void setGroupID(int groupID) {
-        this.postgresPoll.setGroupID(groupID);
-    }
-
-    public int getNumAnswersAllowed() {
-        return postgresPoll.getNumAnswersAllowed();
-    }
-
-    public void setNumAnswersAllowed(int numAnswersAllowed) {
-        this.postgresPoll.setNumAnswersAllowed(numAnswersAllowed);
-    }
-
-    public long getTimeCreated() {
-        return postgresPoll.getTimeCreated();
-    }
-
-    public void setTimeCreated(long timeCreated) {
-        this.postgresPoll.setTimeCreated(timeCreated);
-    }
-
-    public long getTimeUpdated() {
-        return postgresPoll.getTimeUpdated();
-    }
-
-    public void setTimeUpdated(long timeUpdated) {
-        this.postgresPoll.setTimeUpdated(timeUpdated);
-    }
-
-    public long getTimeEnds() {
-        return postgresPoll.getTimeEnds();
-    }
-
-    public void setTimeEnds(long timeEnds) {
-        this.postgresPoll.setTimeEnds(timeEnds);
     }
 
     public List<Answer> getAnswersList() {
@@ -91,5 +29,85 @@ public class Poll {
 
     public void setAnswersList(List<Answer> answersList) {
         this.answersList = answersList;
+    }
+
+    public int getPollID() {
+        return pollID;
+    }
+
+    public void setPollID(int pollID) {
+        this.pollID = pollID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreatorID() {
+        return creatorID;
+    }
+
+    public void setCreatorID(int creatorID) {
+        this.creatorID = creatorID;
+    }
+
+    public int getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(int groupID) {
+        this.groupID = groupID;
+    }
+
+    public int getVotingID() {
+        return votingID;
+    }
+
+    public void setVotingID(int votingID) {
+        this.votingID = votingID;
+    }
+
+    public int getNumAnswersAllowed() {
+        return numAnswersAllowed;
+    }
+
+    public void setNumAnswersAllowed(int numAnswersAllowed) {
+        this.numAnswersAllowed = numAnswersAllowed;
+    }
+
+    public long getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(long timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public long getTimeUpdated() {
+        return timeUpdated;
+    }
+
+    public void setTimeUpdated(long timeUpdated) {
+        this.timeUpdated = timeUpdated;
+    }
+
+    public long getTimeEnds() {
+        return timeEnds;
+    }
+
+    public void setTimeEnds(long timeEnds) {
+        this.timeEnds = timeEnds;
     }
 }
